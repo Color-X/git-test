111215cf273d61a2b471d19e1dfa17aff06c6923
Color-X <2505857792@qq.com>
2022-04-26 16:25:17 +0800

    master 8

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
         System.out.println(12314);
          System.out.println("421");
         System.out.println(12314);
+        System.out.println("421");
+        System.out.println(12314);
     }
 }
