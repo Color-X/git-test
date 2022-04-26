3cd75b9063fb75caa877d989e9c91c638c8f4f28
Color-X <2505857792@qq.com>
2022-04-26 15:50:17 +0800

    mater 6

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -3,11 +3,6 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gi1");
-        System.out.println("hell 2");
-        System.out.println("24211321");
-        System.out.println("test");
-        System.out.println("124141321");
-        System.out.println("124141321");
-        System.out.println("124141321");
+        System.out.println("421");
     }
 }
