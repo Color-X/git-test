ea8cd76957233e509c227217f303cce54c2afacd
Color-X <2505857792@qq.com>
2022-04-26 15:08:35 +0800

    hot-fix 2

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gi1");
         System.out.println("hell 2");
-        System.out.println("hello3");
+        System.out.println("24211321");
     }
 }
