b6b040462e0f75aef9213b50006a2159c1348b08
Color-X <62678537+Color-X@users.noreply.github.com>
2022-04-26 15:59:28 +0800

    Update GitTest.java

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello gi1");
         System.out.println("421");
+        System.out.println("4214214211");
     }
 }
